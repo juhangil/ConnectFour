f74c84a3dc68e2c3a9bf1af6c3b30fb439e3a958
juhangil <tensil94@gmail.com>
2024-07-18 08:52:06 +0900

    [Add] module exercise end
    - Add Board.razor and css for Connect Four game view
    - Add ConnectFourBoardState for Connect Four game logic

--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -1,10 +1,14 @@
 using ConnectFour.Components;
+using ConnectFour.State;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add State
+builder.Services.AddSingleton<ConnectFourBoardState>();
 
 var app = builder.Build();
 
